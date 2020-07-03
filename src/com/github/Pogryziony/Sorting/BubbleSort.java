db07f30894d9570c4f545cc3a362350d460d9559
Pogryziony <Pogryziony98@interia.pl>
2020-07-03 10:29:24 +0200

    Added bubble sort
    Added bubble sort class and method

--- src/com/github/Pogryziony/Sorting/BubbleSort.java
+++ src/com/github/Pogryziony/Sorting/BubbleSort.java
@@ -2,4 +2,17 @@ package com.github.Pogryziony.Sorting;
 
 public class BubbleSort {
 
+	public Integer[] bubbleSort(Integer[] a) {
+		
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length-1; j++) {
+				if (a[j]>a[j+1]) {
+					int temp = a[j+1];
+					a[j+1]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		return a;
+	}
 }
