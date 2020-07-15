0cfc17b0b91aa4256a88963d2ecbfd856fd6a46f
Pogryziony <pogryziony98@interia.pl>
2020-07-15 08:39:53 +0200

    Implementation of Insertion Sort

--- src/com/github/Pogryziony/Sorting/InsertionSort.java
+++ src/com/github/Pogryziony/Sorting/InsertionSort.java
@@ -1,5 +1,35 @@
 package com.github.Pogryziony.Sorting;
 
-public class InsertionSort {
 
+//Time Complexity: O(n*2)
+//Auxiliary Space: O(1)
+public class InsertionSort {
+private int[] array;
+	public static int[] insertionSort(int[] array) {
+		int key;
+		int i;
+		int j;
+		for (i = 1;  i < array.length; i++) {
+			key = array[i];
+			j = i-1;
+			
+			 /* Move elements of array, that are  
+	        greater than key, to one position ahead  
+	        of their current position */
+			
+			while(j >=0 && array[j] > key) {
+				array[j+1] = array[j];
+				j=j-1;
+			}
+			array[j+1]=key;
+		}
+		
+		return array;
+	}
+	
+	public int[] getArray() {
+		return array;
+	}
+
+	
 }
